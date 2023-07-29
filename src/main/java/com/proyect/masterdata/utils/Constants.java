ed1fc1447f457d24eb683ba9a7788c711dc0bfb5
Juan Romero <jryn145@gmail.com>
2023-07-29 13:40:23 -0500

    feature(database): add sale channel entity with CRUD methods and controller

--- src/main/java/com/proyect/masterdata/utils/Constants.java
+++ src/main/java/com/proyect/masterdata/utils/Constants.java
@@ -12,4 +12,5 @@ public class Constants {
     public static final String tableState = "estados";
     public static final String tableSize = "tallas";
     public static final String tableColor = "colores";
+    public static final String tableSaleChannel = "canales_venta";
 }
