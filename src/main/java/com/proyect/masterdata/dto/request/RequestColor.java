3c25e93b811611f4683426380d7223bca68cde0e
Juan Romero <jryn145@gmail.com>
2023-08-02 14:58:13 -0500

    refactor(database): creating request file for each database entity

---
+++ src/main/java/com/proyect/masterdata/dto/request/RequestColor.java
@@ -0,0 +1,16 @@
+package com.proyect.masterdata.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class RequestColor {
+    private Long code;
+    private String name;
+    private boolean status;
+}
