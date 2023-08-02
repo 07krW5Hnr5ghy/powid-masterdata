08294516184ee3fe6291c5c2c7fae2d71d2d5c92
Juan Romero <jryn145@gmail.com>
2023-08-02 17:05:59 -0500

    refactor(database): create interface files for each entity of database

---
+++ src/main/java/com/proyect/masterdata/services/IPaymentState.java
@@ -0,0 +1,20 @@
+package com.proyect.masterdata.services;
+
+import com.proyect.masterdata.dto.DepartmentDTO;
+import com.proyect.masterdata.dto.request.RequestDepartment;
+import com.proyect.masterdata.dto.response.ResponseDelete;
+import com.proyect.masterdata.dto.response.ResponseSuccess;
+import com.proyect.masterdata.exceptions.BadRequestExceptions;
+
+import java.util.List;
+
+public interface IPaymentState {
+    ResponseSuccess save(String name) throws BadRequestExceptions;
+    ResponseSuccess saveAll(List<String> names) throws BadRequestExceptions;
+    DepartmentDTO update(RequestDepartment requestDepartment) throws BadRequestExceptions;
+    ResponseDelete delete(Long code) throws BadRequestExceptions;
+    ResponseDelete deleteAll(List<Long> codes) throws BadRequestExceptions;
+    List<DepartmentDTO> list() throws BadRequestExceptions;
+    DepartmentDTO findByCode(Long code) throws BadRequestExceptions;
+    DepartmentDTO findByName(String name) throws BadRequestExceptions;
+}
