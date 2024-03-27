d48762d093e7a52b8824432b2dd362e7de035064
07krW5Hnr5ghy <jryn145@gmail.com>
2024-03-27 00:01:46 -0500

    refactor(subscription-payment): set supplier Ruc url parameter for supplier product get controller as not required

--- src/main/java/com/proyect/masterdata/controller/SupplierProductController.java
+++ src/main/java/com/proyect/masterdata/controller/SupplierProductController.java
@@ -108,7 +108,7 @@ public class SupplierProductController {
     //@PreAuthorize("hasAnyAuthority('ROLE:STOCK','ROLE:ADMINISTRATION','ROLE:BUSINESS') and hasAuthority('ACCESS:SUPPLIER_PRODUCT_GET')")
     public ResponseEntity<List<SupplierProductDTO>> listSupplierProductFalse(
             @RequestParam("user") String user,
-            @RequestParam("supplierRuc") String supplierRuc
+            @RequestParam(value = "supplierRuc",required = false) String supplierRuc
     ) throws BadRequestExceptions {
         List<SupplierProductDTO> result = iSupplierProduct.listSupplierProductFalse(user,supplierRuc);
         return new ResponseEntity<>(result,HttpStatus.OK);
