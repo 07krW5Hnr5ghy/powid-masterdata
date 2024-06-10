1b778be7fd2633bf51750495b9a23fb09c03913e
07krW5Hnr5ghy <jryn145@gmail.com>
2024-06-10 18:50:31 -0500

    feature(timestamp): add timestamtp sorting and filtering for date timestamps for audits

--- src/main/java/com/proyect/masterdata/services/IAudit.java
+++ src/main/java/com/proyect/masterdata/services/IAudit.java
@@ -6,9 +6,10 @@ import com.proyect.masterdata.exceptions.BadRequestExceptions;
 import com.proyect.masterdata.exceptions.InternalErrorExceptions;
 import org.springframework.data.domain.Page;
 
+import java.util.Date;
 import java.util.concurrent.CompletableFuture;
 
 public interface IAudit {
     CompletableFuture<ResponseSuccess> save(String eventName, String detail, String tokenUser) throws BadRequestExceptions, InternalErrorExceptions;
-    CompletableFuture<Page<AuditDTO>> list(String username,String eventName,String clientRuc,String sort,String sortColumn,Integer pageNumber,Integer pageSize) throws BadRequestExceptions,InternalErrorExceptions;
+    CompletableFuture<Page<AuditDTO>> list(String username,String eventName, String clientRuc, Date registrationStartDate, Date registrationEndDate, Date updateStartDate, Date updateEndDate, String sort, String sortColumn, Integer pageNumber, Integer pageSize) throws BadRequestExceptions,InternalErrorExceptions;
 }
