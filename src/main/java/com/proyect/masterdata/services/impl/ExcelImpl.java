b6777e8a91ff59d80c64d34c43abd103e5e42a28
07krW5Hnr5ghy <jryn145@gmail.com>
2024-09-05 17:14:23 -0500

    refactor(development): excel product route fix

--- src/main/java/com/proyect/masterdata/services/impl/ExcelImpl.java
+++ src/main/java/com/proyect/masterdata/services/impl/ExcelImpl.java
@@ -1314,6 +1314,7 @@ public class ExcelImpl implements IExcel {
                             newProduct.setSku(row.getCell(0).getRichStringCellValue().getString().toUpperCase());
                         }
                         if((i>=1) && (row.getCell(0).getCellType() == NUMERIC) && (ii==0)){
+                            System.out.println(row.getCell(0).getRichStringCellValue());
                             product = productRepository.findBySku(String.valueOf((int) (row.getCell(0).getNumericCellValue())));
                             if(product != null){
                                 throw new BadRequestExceptions(Constants.ErrorProductExists);
@@ -1321,6 +1322,7 @@ public class ExcelImpl implements IExcel {
                             newProduct.setSku(row.getCell(0).getRichStringCellValue().getString().toUpperCase());
                         }
                         if((i>=1)&&(row.getCell(2).getCellType() == STRING) && (ii==2)){
+                            System.out.println(row.getCell(2));
                             model = modelRepository.findByNameAndClientIdAndStatusTrue(row.getCell(2).getRichStringCellValue().getString().toUpperCase(),user.getClientId());
                             if(model == null){
                                 throw new BadRequestExceptions(Constants.ErrorModel);
@@ -1329,6 +1331,7 @@ public class ExcelImpl implements IExcel {
                             newProduct.setModelId(model.getId());
                         }
                         if((i>=1)&&(row.getCell(3).getCellType()==STRING)&&(ii==3)){
+                            System.out.println(row.getCell(3).getRichStringCellValue());
                             color = colorRepository.findByNameAndStatusTrue(row.getCell(3).getRichStringCellValue().getString().toUpperCase());
                             if(color==null){
                                 throw new BadRequestExceptions(Constants.ErrorColor);
@@ -1345,10 +1348,12 @@ public class ExcelImpl implements IExcel {
                             newProduct.setCategoryProductId(categoryProduct.getId());
                         }
                         if((i>=1)&&(row.getCell(5).getCellType()==STRING)&&(ii==5)&&(categoryProduct!=null)){
+                            System.out.println(row.getCell(5).getRichStringCellValue().getString().toUpperCase());
                             size = sizeRepository.findByNameAndStatusTrue(row.getCell(5).getRichStringCellValue().getString().toUpperCase());
                             if(size==null){
                                 throw new BadRequestExceptions(Constants.ErrorSize);
                             }
+
                             if(!Objects.equals(size.getSizeTypeId(), categoryProduct.getSizeTypeId())){
                                 throw new BadRequestExceptions(Constants.ErrorSizeTypeCategoryProduct);
                             }
@@ -1366,7 +1371,7 @@ public class ExcelImpl implements IExcel {
                             newProduct.setSize(size);
                             newProduct.setSizeId(size.getId());
                         }
-                        if((i>=1)&&(row.getCell(5).getCellType()==STRING)&&(ii==6)){
+                        if((i>=1)&&(row.getCell(6).getCellType()==STRING)&&(ii==6)){
                             unitType = unitTypeRepository.findByNameAndStatusTrue(row.getCell(6).getRichStringCellValue().getString().toUpperCase());
                             if(unitType == null){
                                 throw new BadRequestExceptions(Constants.ErrorUnitType);
