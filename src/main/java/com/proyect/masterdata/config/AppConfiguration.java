e394de779a71b60bc7b638155fed0e54cc386be4
07krW5Hnr5ghy <jryn145@gmail.com>
2024-09-24 14:02:49 -0500

    feature(stats): config a bigger thread pool for graphics and reports

--- src/main/java/com/proyect/masterdata/config/AppConfiguration.java
+++ src/main/java/com/proyect/masterdata/config/AppConfiguration.java
@@ -4,6 +4,9 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 @Configuration
 @EnableAsync
@@ -11,5 +14,16 @@ public class AppConfiguration {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public Executor asyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(40);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("AsyncExecutor-");
+        executor.initialize();
+        return executor;
     }
 }
