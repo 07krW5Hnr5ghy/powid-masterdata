96329d54a7931ddb32b2d42b7730a64949fd3a99
07krW5Hnr5ghy <jryn145@gmail.com>
2024-10-20 21:46:27 -0500

    refactor(nplus1): fix excel report for sales category graphic service

---
+++ src/main/java/com/proyect/masterdata/dto/SalesCategoryReportRawDTO.java
@@ -0,0 +1,28 @@
+package com.proyect.masterdata.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class SalesCategoryReportRawDTO {
+    private Long orderId;
+    private Date registrationDate;
+    private Double orderDiscountAmount;
+    private String orderDiscountName;
+    private String closingChannelName;
+    private Long orderItemId;
+    private Integer quantity;
+    private Double orderItemDiscountAmount;
+    private String orderItemDiscountName;
+    private String categoryName;
+    private Double unitSalePrice;
+    private Double orderItemPrice;
+    private String brandName;
+}
