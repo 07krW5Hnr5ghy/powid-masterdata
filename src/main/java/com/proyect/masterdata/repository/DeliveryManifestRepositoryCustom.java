16c6bf7d84bd0d768b77a75294dddfd904b8d009
07krW5Hnr5ghy <jryn145@gmail.com>
2025-02-24 13:00:51 -0500

    feature(models): create delivery manifest custom repostiory

--- src/main/java/com/proyect/masterdata/repository/DeliveryManifestRepositoryCustom.java
+++ src/main/java/com/proyect/masterdata/repository/DeliveryManifestRepositoryCustom.java
@@ -1,7 +1,27 @@
 package com.proyect.masterdata.repository;
 
+import com.proyect.masterdata.domain.DeliveryManifest;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Repository;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
 
 @Repository
 public interface DeliveryManifestRepositoryCustom {
+    Page<DeliveryManifest> searchForDeliveryManifest(
+            UUID clientId,
+            Long manifestNumber,
+            String warehouse,
+            String courier,
+            OffsetDateTime registrationStartDate,
+            OffsetDateTime registrationEndDate,
+            OffsetDateTime updateStartDate,
+            OffsetDateTime updateEndDate,
+            String sort,
+            String sortColumn,
+            Integer pageNumber,
+            Integer pageSize,
+            Boolean status
+    );
 }
