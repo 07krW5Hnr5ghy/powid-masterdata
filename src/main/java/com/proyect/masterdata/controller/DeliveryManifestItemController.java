e3014b27f73d3a7e9423068400a23af52fd6e5ec
07krW5Hnr5ghy <jryn145@gmail.com>
2025-02-24 21:32:54 -0500

    feature(models): implement method for list delivery manifest and delivery manifest item with controllers

--- src/main/java/com/proyect/masterdata/controller/DeliveryManifestItemController.java
+++ src/main/java/com/proyect/masterdata/controller/DeliveryManifestItemController.java
@@ -1,14 +1,18 @@
 package com.proyect.masterdata.controller;
 
+import com.proyect.masterdata.dto.DeliveryManifestItemDTO;
 import com.proyect.masterdata.dto.response.ResponseSuccess;
 import com.proyect.masterdata.exceptions.BadRequestExceptions;
 import com.proyect.masterdata.exceptions.InternalErrorExceptions;
 import com.proyect.masterdata.services.IDeliveryManifestItem;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.OffsetDateTime;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
@@ -26,5 +30,54 @@ public class DeliveryManifestItemController {
     ) throws BadRequestExceptions, InternalErrorExceptions, ExecutionException, InterruptedException {
         CompletableFuture<ResponseSuccess> result = iDeliveryManifestItem.updateDeliveryManifestItem(deliveryManifestItemId,user);
         return new ResponseEntity<>(result.get(), HttpStatus.OK);
+    }
+    @GetMapping()
+    public ResponseEntity<Page<DeliveryManifestItemDTO>> list(
+            @RequestParam(value = "user", required = true) String user,
+            @RequestParam(value = "quantity",required = false) Integer quantity,
+            @RequestParam(value = "collected",required = false) Boolean collected,
+            @RequestParam(value = "orderNumber",required = false) Long orderNumber,
+            @RequestParam(value = "manifestNumber",required = false) Long manifestNumber,
+            @RequestParam(value = "color",required = false) String color,
+            @RequestParam(value = "size",required = false) String size,
+            @RequestParam(value = "model",required = false) String model,
+            @RequestParam(value = "supplier",required = false) String supplier,
+            @RequestParam(value = "brand",required = false) String brand,
+            @RequestParam(value = "deliveryStatus",required = false) String deliveryStatus,
+            @RequestParam(value = "courier",required = false) String courier,
+            @RequestParam(value = "warehouse",required = false) String warehouse,
+            @RequestParam(value = "registrationStartDate",required = false) @DateTimeFormat(iso=DateTimeFormat.ISO.DATE) OffsetDateTime registrationStartDate,
+            @RequestParam(value = "registrationEndDate",required = false) @DateTimeFormat(iso=DateTimeFormat.ISO.DATE) OffsetDateTime registrationEndDate,
+            @RequestParam(value = "updateStartDate",required = false) @DateTimeFormat(iso=DateTimeFormat.ISO.DATE) OffsetDateTime updateStartDate,
+            @RequestParam(value = "updateEndDate",required = false) @DateTimeFormat(iso=DateTimeFormat.ISO.DATE) OffsetDateTime updateEndDate,
+            @RequestParam(value = "sort", required = false) String sort,
+            @RequestParam(value = "sortColumn", required = false) String sortColumn,
+            @RequestParam(value = "pageNumber", required = true) Integer pageNumber,
+            @RequestParam(value = "pageSize", required = true) Integer pageSize
+    ) throws BadRequestExceptions, InternalErrorExceptions, ExecutionException, InterruptedException {
+        CompletableFuture<Page<DeliveryManifestItemDTO>> result = iDeliveryManifestItem.list(
+                user,
+                quantity,
+                collected,
+                orderNumber,
+                manifestNumber,
+                color,
+                size,
+                model,
+                supplier,
+                brand,
+                deliveryStatus,
+                courier,
+                warehouse,
+                registrationStartDate,
+                registrationEndDate,
+                updateStartDate,
+                updateEndDate,
+                sort,
+                sortColumn,
+                pageNumber,
+                pageSize
+        );
+        return new ResponseEntity<>(result.get(),HttpStatus.OK);
     }
 }
--- src/main/java/com/proyect/masterdata/services/impl/DeliveryManifestItemImpl.java
+++ src/main/java/com/proyect/masterdata/services/impl/DeliveryManifestItemImpl.java
@@ -1,6 +1,7 @@
 package com.proyect.masterdata.services.impl;
 
 import com.proyect.masterdata.domain.*;
+import com.proyect.masterdata.dto.DeliveryManifestItemDTO;
 import com.proyect.masterdata.dto.request.RequestDeliveryManifestItem;
 import com.proyect.masterdata.dto.request.RequestStockTransactionItem;
 import com.proyect.masterdata.dto.response.ResponseSuccess;
@@ -11,9 +12,13 @@ import com.proyect.masterdata.services.*;
 import com.proyect.masterdata.utils.Constants;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.stereotype.Service;
 
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
@@ -32,6 +37,8 @@ public class DeliveryManifestItemImpl implements IDeliveryManifestItem{
     private final IGeneralStock iGeneralStock;
     private final IAudit iAudit;
     private final IStockTransaction iStockTransaction;
+    private final DeliveryManifestItemRepositoryCustom deliveryManifestItemRepositoryCustom;
+    private final IUtil iUtil;
     @Override
     public CompletableFuture<DeliveryManifestItem> save(
             RequestDeliveryManifestItem requestDeliveryManifestItem,
@@ -158,6 +165,80 @@ public class DeliveryManifestItemImpl implements IDeliveryManifestItem{
                 log.error(e.getMessage());
                 throw new InternalErrorExceptions(Constants.InternalErrorExceptions);
             }
+        });
+    }
+
+    @Override
+    public CompletableFuture<Page<DeliveryManifestItemDTO>> list(
+            String user,
+            Integer quantity,
+            Boolean collected,
+            Long orderNumber,
+            Long manifestNumber,
+            String color,
+            String size,
+            String model,
+            String supplier,
+            String brand,
+            String deliveryStatus,
+            String courier,
+            String warehouse,
+            OffsetDateTime registrationStartDate,
+            OffsetDateTime registrationEndDate,
+            OffsetDateTime updateStartDate,
+            OffsetDateTime updateEndDate,
+            String sort,
+            String sortColumn,
+            Integer pageNumber,
+            Integer pageSize) {
+        return CompletableFuture.supplyAsync(()->{
+            Page<DeliveryManifestItem> deliveryManifestItemPage;
+            UUID clientId;
+            try{
+                clientId = userRepository.findByUsernameAndStatusTrue(user.toUpperCase()).getClientId();
+                deliveryManifestItemPage = deliveryManifestItemRepositoryCustom.searchForDeliveryManifestItem(
+                        clientId,
+                        quantity,
+                        collected,
+                        orderNumber,
+                        manifestNumber,
+                        color,
+                        size,
+                        model,
+                        supplier,
+                        brand,
+                        deliveryStatus,
+                        courier,
+                        warehouse,
+                        registrationStartDate,
+                        registrationEndDate,
+                        updateStartDate,
+                        updateEndDate,
+                        sort,
+                        sortColumn,
+                        pageNumber,
+                        pageSize
+                );
+            }catch (RuntimeException e){
+                log.error(e.getMessage());
+                e.printStackTrace();
+                throw new BadRequestExceptions(Constants.ResultsFound);
+            }
+            if(deliveryManifestItemPage.isEmpty()){
+                return new PageImpl<>(Collections.emptyList());
+            }
+            List<DeliveryManifestItemDTO> deliveryManifestItemDTOS = deliveryManifestItemPage.stream().map(deliveryManifestItem -> DeliveryManifestItemDTO.builder()
+                    .id(deliveryManifestItem.getId())
+                    .phone(deliveryManifestItem.getOrderItem().getOrdering().getCustomer().getPhone())
+                    .district(deliveryManifestItem.getOrderItem().getOrdering().getCustomer().getDistrict().getName())
+                    .manifestNumber(deliveryManifestItem.getDeliveryManifest().getManifestNumber())
+                    .orderNumber(deliveryManifestItem.getOrderItem().getOrdering().getOrderNumber())
+                    .quantity(deliveryManifestItem.getQuantity())
+                    .customer(deliveryManifestItem.getOrderItem().getOrdering().getCustomer().getName())
+                    .skuInventory(iUtil.buildInventorySku(deliveryManifestItem.getSupplierProduct()))
+                    .skuProduct(iUtil.buildProductSku(deliveryManifestItem.getSupplierProduct().getProduct()))
+                    .build()).toList();
+            return new PageImpl<>(deliveryManifestItemDTOS,deliveryManifestItemPage.getPageable(),deliveryManifestItemPage.getTotalElements());
         });
     }
 }
