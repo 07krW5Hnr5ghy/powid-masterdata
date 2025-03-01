e2910fb308613368327e96fcfe2d3131c04a65a5
07krW5Hnr5ghy <jryn145@gmail.com>
2025-03-01 01:20:47 -0500

    feature(models): controller for order cotacted repository

--- src/main/java/com/proyect/masterdata/controller/OrderContactedController.java
+++ src/main/java/com/proyect/masterdata/controller/OrderContactedController.java
@@ -1,13 +1,17 @@
 package com.proyect.masterdata.controller;
 
+import com.proyect.masterdata.dto.OrderContactedDTO;
 import com.proyect.masterdata.dto.response.ResponseSuccess;
 import com.proyect.masterdata.exceptions.BadRequestExceptions;
 import com.proyect.masterdata.services.IOrderContacted;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.OffsetDateTime;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
@@ -24,6 +28,34 @@ public class OrderContactedController {
             @RequestParam("tokenUser") String tokenUser
     ) throws BadRequestExceptions, ExecutionException, InterruptedException {
         CompletableFuture<ResponseSuccess> result = iOrderContacted.markContacted(orderId,tokenUser);
+        return new ResponseEntity<>(result.get(), HttpStatus.OK);
+    }
+    @GetMapping(value = "list")
+    //@PreAuthorize("hasAnyAuthority('ROLE:ADMINISTRATION','ROLE:MARKETING','ROLE:STOCK') and hasAuthority('ACCESS:COLOR_GET')")
+    public ResponseEntity<Page<OrderContactedDTO>> list(
+            @RequestParam(value = "user") String user,
+            @RequestParam(value = "orderNumber", required = false) Long orderNumber,
+            @RequestParam(value = "contacted", required = false) Boolean contacted,
+            @RequestParam(value = "registrationStartDate",required = false) @DateTimeFormat(iso=DateTimeFormat.ISO.DATE) OffsetDateTime registrationStartDate,
+            @RequestParam(value = "registrationEndDate",required = false) @DateTimeFormat(iso=DateTimeFormat.ISO.DATE) OffsetDateTime registrationEndDate,
+            @RequestParam(value = "updateStartDate",required = false) @DateTimeFormat(iso=DateTimeFormat.ISO.DATE) OffsetDateTime updateStartDate,
+            @RequestParam(value = "updateEndDate",required = false) @DateTimeFormat(iso=DateTimeFormat.ISO.DATE) OffsetDateTime updateEndDate,
+            @RequestParam(value = "sort", required = false) String sort,
+            @RequestParam(value = "sortColumn", required = false) String sortColumn,
+            @RequestParam("pageNumber") Integer pageNumber,
+            @RequestParam("pageSize") Integer pageSize) throws BadRequestExceptions, ExecutionException, InterruptedException {
+        CompletableFuture<Page<OrderContactedDTO>> result = iOrderContacted.list(
+                user,
+                orderNumber,
+                contacted,
+                registrationStartDate,
+                registrationEndDate,
+                updateStartDate,
+                updateEndDate,
+                sort,
+                sortColumn,
+                pageNumber,
+                pageSize);
         return new ResponseEntity<>(result.get(), HttpStatus.OK);
     }
 }
