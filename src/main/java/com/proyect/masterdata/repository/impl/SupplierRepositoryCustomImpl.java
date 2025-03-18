4afdba7f51c00c3a0f910801558439ed65dd71ad
07krW5Hnr5ghy <jryn145@gmail.com>
2025-03-18 18:49:17 -0500

    refactor(models): fix status check in supplier repository custom

--- src/main/java/com/proyect/masterdata/repository/impl/SupplierRepositoryCustomImpl.java
+++ src/main/java/com/proyect/masterdata/repository/impl/SupplierRepositoryCustomImpl.java
@@ -149,11 +149,11 @@ public class SupplierRepositoryCustomImpl implements SupplierRepositoryCustom {
             conditions.add(criteriaBuilder.and(criteriaBuilder.equal(itemRoot.get("clientId"), clientId)));
         }
 
-        if (status) {
+        if(Boolean.TRUE.equals(status)) {
             conditions.add(criteriaBuilder.and(criteriaBuilder.isTrue(itemRoot.get("status"))));
         }
 
-        if (!status) {
+        if(Boolean.FALSE.equals(status)) {
             conditions.add(criteriaBuilder.and(criteriaBuilder.isFalse(itemRoot.get("status"))));
         }
 
