acad8500b09b7c04aafbdb9127da0e4e3123da2a
07krW5Hnr5ghy <jryn145@gmail.com>
2025-04-07 12:19:11 -0500

    refactor(models): filter by payment state due payment amount in courier profile

--- src/main/java/com/proyect/masterdata/services/impl/DeliveryManifestItemImpl.java
+++ src/main/java/com/proyect/masterdata/services/impl/DeliveryManifestItemImpl.java
@@ -366,33 +366,35 @@ public class DeliveryManifestItemImpl implements IDeliveryManifestItem{
                 }
                 System.out.println(uniqueOrderNumbersUnCollected);
                 for(Ordering order:ordersUnCollected){
-                    List<OrderItem> orderItems = orderItemRepository.findAllByOrderIdAndStatusTrue(order.getId());
-                    double saleAmount = 0.00;
-                    for(OrderItem orderItem : orderItems){
-                        ProductPrice productPrice = productPriceRepository.findByProductIdAndStatusTrue(orderItem.getProductId());
-                        if(Objects.equals(orderItem.getDiscount().getName(), "PORCENTAJE")) {
-                            saleAmount += (productPrice.getUnitSalePrice() * orderItem.getDeliveredProducts()) - ((productPrice.getUnitSalePrice() * orderItem.getDeliveredProducts()) * (orderItem.getDiscountAmount() / 100));
+                    if(Objects.equals(order.getOrderPaymentState().getName(), "POR RECAUDAR")){
+                        List<OrderItem> orderItems = orderItemRepository.findAllByOrderIdAndStatusTrue(order.getId());
+                        double saleAmount = 0.00;
+                        for(OrderItem orderItem : orderItems){
+                            ProductPrice productPrice = productPriceRepository.findByProductIdAndStatusTrue(orderItem.getProductId());
+                            if(Objects.equals(orderItem.getDiscount().getName(), "PORCENTAJE")) {
+                                saleAmount += (productPrice.getUnitSalePrice() * orderItem.getDeliveredProducts()) - ((productPrice.getUnitSalePrice() * orderItem.getDeliveredProducts()) * (orderItem.getDiscountAmount() / 100));
+                            }
+                            if(Objects.equals(orderItem.getDiscount().getName(), "MONTO")){
+                                saleAmount += (productPrice.getUnitSalePrice() * orderItem.getDeliveredProducts()) - orderItem.getDiscountAmount();
+                            }
+                            if(Objects.equals(orderItem.getDiscount().getName(), "NO APLICA")){
+                                saleAmount += (productPrice.getUnitSalePrice() * orderItem.getDeliveredProducts());
+                            }
                         }
-                        if(Objects.equals(orderItem.getDiscount().getName(), "MONTO")){
-                            saleAmount += (productPrice.getUnitSalePrice() * orderItem.getDeliveredProducts()) - orderItem.getDiscountAmount();
+                        System.out.println("Sale Amount : -> "+saleAmount);
+                        double totalDuePayment=0;
+                        if(Objects.equals(order.getDiscount().getName(), "PORCENTAJE")){
+                            totalDuePayment = (saleAmount-((saleAmount)*(order.getDiscountAmount()/100))+order.getDeliveryAmount())-order.getAdvancedPayment();
                         }
-                        if(Objects.equals(orderItem.getDiscount().getName(), "NO APLICA")){
-                            saleAmount += (productPrice.getUnitSalePrice() * orderItem.getDeliveredProducts());
+                        if(Objects.equals(order.getDiscount().getName(), "MONTO")){
+                            totalDuePayment = (saleAmount-order.getDiscountAmount()+order.getDeliveryAmount())-order.getAdvancedPayment();
                         }
+                        if(Objects.equals(order.getDiscount().getName(), "NO APLICA")){
+                            totalDuePayment = (saleAmount+order.getDeliveryAmount())-order.getAdvancedPayment();
+                        }
+                        System.out.println(totalDuePayment);
+                        unCollectedAmount+=totalDuePayment;
                     }
-                    System.out.println("Sale Amount : -> "+saleAmount);
-                    double totalDuePayment=0;
-                    if(Objects.equals(order.getDiscount().getName(), "PORCENTAJE")){
-                        totalDuePayment = (saleAmount-((saleAmount)*(order.getDiscountAmount()/100))+order.getDeliveryAmount())-order.getAdvancedPayment();
-                    }
-                    if(Objects.equals(order.getDiscount().getName(), "MONTO")){
-                        totalDuePayment = (saleAmount-order.getDiscountAmount()+order.getDeliveryAmount())-order.getAdvancedPayment();
-                    }
-                    if(Objects.equals(order.getDiscount().getName(), "NO APLICA")){
-                        totalDuePayment = (saleAmount+order.getDeliveryAmount())-order.getAdvancedPayment();
-                    }
-                    System.out.println(totalDuePayment);
-                    unCollectedAmount+=totalDuePayment;
                 }
                 System.out.println(unCollectedAmount);
                 return CourierProfileDTO.builder()
