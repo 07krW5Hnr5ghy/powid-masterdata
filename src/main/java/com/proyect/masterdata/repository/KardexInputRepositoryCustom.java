2e29eed59767c97465675e316f4e62a411e1507a
07krW5Hnr5ghy <jryn145@gmail.com>
2025-04-15 17:04:07 -0500

    refactor(kardex): add interface for custom repository in kardex input

---
+++ src/main/java/com/proyect/masterdata/repository/KardexInputRepositoryCustom.java
@@ -0,0 +1,30 @@
+package com.proyect.masterdata.repository;
+
+import com.proyect.masterdata.domain.KardexInput;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Repository;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Repository
+public interface KardexInputRepositoryCustom {
+    Page<KardexInput> searchForKardexInput(
+            Long clientId,
+            Integer quantity,
+            Long lotNumber,
+            String product,
+            UUID productId,
+            String username,
+            String warehouse,
+            OffsetDateTime registrationStartDate,
+            OffsetDateTime registrationEndDate,
+            OffsetDateTime updateStartDate,
+            OffsetDateTime updateEndDate,
+            String sort,
+            String sortColumn,
+            Integer pageNumber,
+            Integer pageSize,
+            Boolean status
+    );
+}
