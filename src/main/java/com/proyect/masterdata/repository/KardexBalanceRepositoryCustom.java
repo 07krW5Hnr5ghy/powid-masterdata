39cb8e01f607fd3605283af34420b3a11bbefbfa
07krW5Hnr5ghy <jryn145@gmail.com>
2025-04-17 22:38:37 -0500

    refactor(kardex): add kardex balance custom repository and controller

---
+++ src/main/java/com/proyect/masterdata/repository/KardexBalanceRepositoryCustom.java
@@ -0,0 +1,28 @@
+package com.proyect.masterdata.repository;
+
+import com.proyect.masterdata.domain.KardexBalance;
+import org.springframework.data.domain.Page;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+public interface KardexBalanceRepositoryCustom {
+    Page<KardexBalance> searchForKardexBalance(
+            UUID clientId,
+            Integer quantity,
+            Long lotNumber,
+            String product,
+            UUID productId,
+            String username,
+            String warehouse,
+            Double unitPrice,
+            OffsetDateTime registrationStartDate,
+            OffsetDateTime registrationEndDate,
+            OffsetDateTime updateStartDate,
+            OffsetDateTime updateEndDate,
+            String sort,
+            String sortColumn,
+            Integer pageNumber,
+            Integer pageSize
+    );
+}
--- src/main/java/com/proyect/masterdata/services/impl/KardexBalanceImpl.java
+++ src/main/java/com/proyect/masterdata/services/impl/KardexBalanceImpl.java
@@ -1,25 +1,36 @@
 package com.proyect.masterdata.services.impl;
 
 import com.proyect.masterdata.domain.KardexBalance;
+import com.proyect.masterdata.dto.KardexBalanceDTO;
 import com.proyect.masterdata.dto.request.RequestKardexBalance;
 import com.proyect.masterdata.exceptions.BadRequestExceptions;
 import com.proyect.masterdata.exceptions.InternalErrorExceptions;
 import com.proyect.masterdata.repository.KardexBalanceRepository;
+import com.proyect.masterdata.repository.KardexBalanceRepositoryCustom;
+import com.proyect.masterdata.repository.UserRepository;
 import com.proyect.masterdata.services.IKardexBalance;
+import com.proyect.masterdata.services.IUtil;
 import com.proyect.masterdata.utils.Constants;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.stereotype.Service;
 
 import java.time.OffsetDateTime;
+import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 @RequiredArgsConstructor
 @Log4j2
 public class KardexBalanceImpl implements IKardexBalance {
     private final KardexBalanceRepository kardexBalanceRepository;
+    private final KardexBalanceRepositoryCustom kardexBalanceRepositoryCustom;
+    private final UserRepository userRepository;
+    private final IUtil iUtil;
     @Override
     public KardexBalance save(RequestKardexBalance requestKardexBalance) throws BadRequestExceptions, InternalErrorExceptions {
         KardexBalance kardexBalanceResult = null;
@@ -67,5 +78,62 @@ public class KardexBalanceImpl implements IKardexBalance {
             log.error(e.getMessage());
             throw new InternalErrorExceptions(Constants.InternalErrorExceptions);
         }
+    }
+
+    @Override
+    public CompletableFuture<Page<KardexBalanceDTO>> list(String user, Integer quantity, Long lotNumber, String product, UUID productId, String username, String warehouse, Double unitPrice, OffsetDateTime registrationStartDate, OffsetDateTime registrationEndDate, OffsetDateTime updateStartDate, OffsetDateTime updateEndDate, String sort, String sortColumn, Integer pageNumber, Integer pageSize) throws InternalErrorExceptions {
+        return CompletableFuture.supplyAsync(()->{
+            Page<KardexBalance> kardexBalancePage;
+            UUID clientId;
+
+            try {
+                clientId = userRepository.findByUsernameAndStatusTrue(user.toUpperCase()).getClientId();
+                kardexBalancePage = kardexBalanceRepositoryCustom.searchForKardexBalance(
+                        clientId,
+                        quantity,
+                        lotNumber,
+                        product,
+                        productId,
+                        username,
+                        warehouse,
+                        unitPrice,
+                        registrationStartDate,
+                        registrationEndDate,
+                        updateStartDate,
+                        updateEndDate,
+                        sort,
+                        sortColumn,
+                        pageNumber,
+                        pageSize);
+            } catch (RuntimeException e) {
+                log.error(e.getMessage());
+                e.printStackTrace();
+                throw new BadRequestExceptions(Constants.ResultsFound);
+            }
+
+            if (kardexBalancePage.isEmpty()) {
+                return new PageImpl<>(Collections.emptyList());
+            }
+
+            List<KardexBalanceDTO> kardexBalanceDTOs = kardexBalancePage.getContent().stream()
+                    .map(kardexBalance -> KardexBalanceDTO.builder()
+                            .id(kardexBalance.getId())
+                            .user(kardexBalance.getUser().getUsername())
+                            .quantity(kardexBalance.getRemainingQuantity())
+                            .product(kardexBalance.getProduct().getName())
+                            .productSku(iUtil.buildProductSku(kardexBalance.getProduct()))
+                            .categoryProduct(kardexBalance.getProduct().getSubCategoryProduct().getCategoryProduct().getName())
+                            .subCategoryProduct(kardexBalance.getProduct().getSubCategoryProduct().getName())
+                            .model(kardexBalance.getProduct().getModel().getName())
+                            .color(kardexBalance.getProduct().getColor().getName())
+                            .size(kardexBalance.getProduct().getSize().getName())
+                            .registrationDate(kardexBalance.getRegistrationDate())
+                            .lotNumber(kardexBalance.getLotNumber())
+                            .build())
+                    .toList();
+
+            return new PageImpl<>(kardexBalanceDTOs, kardexBalancePage.getPageable(),
+                    kardexBalancePage.getTotalElements());
+        });
     }
 }
